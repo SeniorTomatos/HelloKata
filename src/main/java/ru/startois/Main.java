ba92490bc21be32d547887c09cfc63cf840ac4d4
SeniorTomatos <dehuckoo5@gmail.com>
2023-09-03 22:53:40 +0300

    Hello Kata

--- src/main/java/ru/startois/Main.java
+++ src/main/java/ru/startois/Main.java
@@ -1,5 +1,7 @@
-package ru.startois;public class Main {
+package ru.startois;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello Kata!");
     }
 }
